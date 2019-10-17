0742d12c7fe2c12950991369b619eebe8df1af22
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 12:01:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -274,6 +274,6 @@ end
 def long_name_steals_a_ton?
   max_steals = 0
   game_hash[:home][:players].each_with_index{|players, index|
-    if game_hash[:home][:players][index]
+    if max_steals < game_hash[:home][:players][index]
   }
 end
