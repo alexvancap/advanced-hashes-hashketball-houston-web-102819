6e1dd95f0952a2ecfb53b041eb4a7b57e10a8e47
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 11:20:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,6 +205,6 @@ end
 def big_shoe_rebounds
   game_hash[:home][:players].each_with_index do {|players, index|
     biggest_feet = 0
-    if game_hash’[:home][:players][index][:shoe]
+    if game_hash’[:home][:players][index][:shoe] >= biggest_feet
   }
 end
