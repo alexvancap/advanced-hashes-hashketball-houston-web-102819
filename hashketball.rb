8b3ef4c32244fbacd72910f3c24905fc256e4f95
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 11:06:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,7 +177,7 @@ def player_numbers (team_name)
   game_hash.each{|items, hash|
     game_hash[:home][:players].each_with_index{|players, index|
       if team_name = game_hash[:home][:team_name]
-
+        numbers << game_hash[:home][:players][index]
     }
   }
 end
