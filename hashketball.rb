8f3a7848f681d080d6d456e2bf8f676817ab2dbe
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 11:44:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -264,6 +264,7 @@ def player_with_longest_name
     elsif longest_name > game_hash[:away][:players][index][:player_name].length
       longest_name = game_hash[:away][:players][index][:player_name].length
       longest_name_person = game_hash[:home][:players][index]
+    end
   }
 end
 
